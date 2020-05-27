95d2191c3a0bd1e3bc9ba3988f74bf23ddf5bb11
松井誠太 <>
2020-05-28 01:01:56 +0900

    1

---
+++ テスト2.php
@@ -0,0 +1,19 @@
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>HTML入門-テキストボックスの作成</title>
+</head>
+<body>
+<form action="#" method="post">
+	<p>ユーザー名：<br>
+	<input type="text" name="name"></p>
+  <textarea name="msg" cols=40 rows=4>
+ あいうえお
+   </textarea>
+	 <img src="test.png" alt="カエルのイラスト">
+
+
+	<p><input type="button" value="確認する" id="button2"></p>
+</form>
+</body>
+</html>
